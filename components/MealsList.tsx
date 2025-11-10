fe3260bf0c1f120e048731142778d1cf2ff2e22c
xCHLOMIx <gutabarwaa@gmail.com>
2025-11-10 18:42:11 +0200

    style #1: Made the meal list responsive

--- components/MealsList.tsx
+++ components/MealsList.tsx
@@ -24,7 +24,7 @@ const MealsList: React.FC = () => {
 
     return (
         <>
-            <div className='grid grid-cols-4 w-full gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 w-full gap-4'>
                 {meals.map((meal) => (
                     <MealCard meal={meal} key={meal.createdAt} />
                 ))}
