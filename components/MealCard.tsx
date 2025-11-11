fe0b0a1440991bcbb7372a1145214c0d9e02afb5
xCHLOMIx <gutabarwaa@gmail.com>
2025-11-11 18:28:41 +0200

    feat: Added pop-up for delet confirmation

--- components/MealCard.tsx
+++ components/MealCard.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import React, { ReactNode, useEffect, useRef, useState } from 'react'
 import { FaStar, FaTag } from 'react-icons/fa'
 import { IoMdMore } from 'react-icons/io'
+import SecondaryButton from './SecondaryButton'
 
 const MealCard: React.FC<{ meal: MealInterface }> = ({ meal }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const dropdownRef = useRef<any>(null)
+  const dropdownRef = useRef<any>(null);
+  const [popUp, setPopUp] = useState<boolean>(false);
 
   useEffect(() => {
     const handleClickedOutside = (event: Event) => {
@@ -17,13 +19,27 @@ const MealCard: React.FC<{ meal: MealInterface }> = ({ meal }) => {
 
     document.addEventListener("mousedown", handleClickedOutside)
 
-    return(() => {
+    return (() => {
       removeEventListener("mousedown", handleClickedOutside)
     })
   }, [])
 
   return (
     <div className='w-full flex flex-col gap-6 items-start'>
+      <div className={`fixed left-0 z-50 ${popUp ? "flex" : "hidden"} justify-center pt-[132px] bg-[#C6C2C285] w-full h-screen top-0`}>
+        <div className='bg-white w-full max-w-[564px] custom-shadow-2 rounded-[20px] h-max px-11 py-12 flex flex-col items-center'>
+          <h1 className='text-tertiary text-[32px] font-bold'>Delete Meal</h1>
+          <p className='text-[#9E9E9E] mt-3'>Are you sure you want to delete this meal? Actions cannot be reversed.</p>
+          <div className='grid grid-cols-2 gap-3 w-full my-[18px]'>
+            <SecondaryButton styles='w-full p-3 text-sm rounded-xl text-center'>
+              <span className='text-center w-full font-bold'>Yes</span>
+            </SecondaryButton>
+            <button onClick={() => setPopUp(false)} className='food-outline-button text-sm w-full'>
+              <span className='text-center w-full font-bold'>Cancel</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div className='w-full h-60 relative overflow-hidden rounded-2xl'>
         <div className='food-badge'>
           <FaTag />
@@ -42,20 +58,25 @@ const MealCard: React.FC<{ meal: MealInterface }> = ({ meal }) => {
           </div>
         </div>
         <div
-          onClick={() => isOpen ? setIsOpen(false) : setIsOpen(true)}
+          onClick={() => setIsOpen(true)}
           className='self-start place-self-end relative hover:bg-black/10 py-1 rounded-md cursor-pointer'
         >
           <IoMdMore className='size-6' />
           <div ref={dropdownRef} className={`${isOpen ? "flex" : "hidden"} flex-col bg-white shadow rounded-xl p-2 mt-2 absolute z-40 right-0`}>
             <div className='pl-2 pr-3 py-1 hover:bg-gray-100 transition duration-150 rounded-lg'>Edit</div>
-            <div className='text-red-500 pl-2 pr-3 py-1 hover:bg-red-50 transition duration-150 rounded-lg'>Delete</div>
+            <div
+              onClick={() => { setPopUp(true); setIsOpen(false) }}
+              className='text-red-500 pl-2 pr-3 py-1 hover:bg-red-50 transition duration-150 rounded-lg'
+            >
+              Delete
+            </div>
           </div>
         </div>
       </div>
       <div className={`text-lg font-bold px-3 p-1 rounded-xl ${meal.open ? "text-custom-green bg-custom-green/20" : "text-primary bg-primary/20"}`}>
         {meal.open ? "Open" : "Closed"}
       </div>
-    </div>
+    </div >
   )
 }
 
--- components/SecondaryButton.tsx
+++ components/SecondaryButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SecondaryButton: React.FC<ButtonProps> = ({ children, type, styles }) => {
     return (
-        <button type={type} className={`food-secondary-button custom-shadow custom-shadow-2 ${styles}`}>
+        <button type={type} className={`food-secondary-button custom-shadow ${styles}`}>
             {children}
         </button>
     )
