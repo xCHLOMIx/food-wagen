5cefb062f814aa340fcd83b5537da40a908cdf58
xCHLOMIx <gutabarwaa@gmail.com>
2025-11-11 12:46:01 +0200

    fix: Fixed the image optimization error (added the sizes prop on Image)

--- components/MealCard.tsx
+++ components/MealCard.tsx
@@ -1,10 +1,12 @@
 import { MealInterface } from '@/lib/types'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { FaStar, FaTag } from 'react-icons/fa'
 import { IoMdMore } from 'react-icons/io'
 
 const MealCard: React.FC<{ meal: MealInterface }> = ({ meal }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <div className='w-full flex flex-col gap-6 items-start'>
       <div className='w-full h-60 relative overflow-hidden rounded-2xl'>
@@ -12,11 +14,11 @@ const MealCard: React.FC<{ meal: MealInterface }> = ({ meal }) => {
           <FaTag />
           <span>${meal.Price}</span>
         </div>
-        <Image src={meal.avatar} alt={meal.name} fill className="object-cover" />
+        <Image src={meal.avatar} alt={meal.name} sizes='240px' fill className="object-cover" />
       </div>
       <div className='grid grid-cols-5 gap-4 w-full items-center'>
         <div className='size-13 rounded-lg overflow-hidden relative'>
-          <Image src={meal.logo} alt={meal.name} fill className="object-cover" />
+          <Image src={meal.logo} alt={meal.name} sizes='240px' fill className="object-cover" />
         </div>
         <div className='w-full col-span-3'>
           <h6 className='font-bold truncate w-11/12 text-foreground'>{meal.name}</h6>
@@ -24,11 +26,18 @@ const MealCard: React.FC<{ meal: MealInterface }> = ({ meal }) => {
             <FaStar className='' /><span>{meal.rating}</span>
           </div>
         </div>
-        <div className='self-start place-self-end hover:bg-black/10 py-1 rounded-md cursor-pointer'>
+        <div
+          onClick={() => isOpen ? setIsOpen(false) : setIsOpen(true)}
+          className='self-start place-self-end relative hover:bg-black/10 py-1 rounded-md cursor-pointer'
+        >
           <IoMdMore className='size-6' />
+          <div className={`${isOpen ? "flex" : "hidden"} flex-col bg-white shadow rounded-xl p-2 mt-2 absolute z-40 right-0`}>
+            <div className='pl-2 pr-3 py-1 hover:bg-gray-100 transition duration-150 rounded-lg'>Edit</div>
+            <div className='text-red-500 pl-2 pr-3 py-1 hover:bg-red-50 transition duration-150 rounded-lg'>Delete</div>
+          </div>
         </div>
       </div>
-      <div className={`text-lg font-bold px-3 p-1 rounded-xl ${meal.open ? "text-custom-green bg-custom-green/20": "text-primary bg-primary/20"}`}>
+      <div className={`text-lg font-bold px-3 p-1 rounded-xl ${meal.open ? "text-custom-green bg-custom-green/20" : "text-primary bg-primary/20"}`}>
         {meal.open ? "Open" : "Closed"}
       </div>
     </div>
